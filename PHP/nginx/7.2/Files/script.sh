a263ecc257f79da7237fcf365c04b99e9e69d934
Kaio Goncalves Carvalho <kaio.carvalho@pravaler.com.br>
2018-07-12 21:51:16 -0300

    Correções em vários docker files e adaptações

---
+++ PHP/nginx/7.2/Files/script.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if  [ -f /etc/nginx/sites-available/site.conf ];
+then
+    sed -E -i "s#(<ROOT>)#$ROOT#g" /etc/nginx/sites-available/site.conf
+    sed -E -i "s#(<URL>)#$URL#g" /etc/nginx/sites-available/site.conf
+    sed -E -i "s#(<VOLUME>)#$VOLUME#g" /etc/nginx/sites-available/site.conf
+    mv /etc/nginx/sites-available/site.conf /etc/nginx/sites-available/site.conf
+    ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
+fi
+
+service php7.2-fpm start
+
+nginx
+
+#Retorna um terminal
+/bin/bash
