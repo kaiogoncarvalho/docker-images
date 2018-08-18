5be1b76ed1c025132f9aeefe63738f7ab9f23d19
Kaio Goncalves Carvalho <kaio.carvalho@pravaler.com.br>
2018-08-17 22:12:16 -0300

    Atualização nas imagens e correções e inclusões

--- Ubuntu/Xenial Xerus - 16.04/Nginx/PHP/7.2/Image/Files/script.sh
+++ Ubuntu/Xenial Xerus - 16.04/Nginx/PHP/7.2/Image/Files/script.sh
@@ -5,7 +5,18 @@ then
     sed -E -i "s#(<ROOT>)#$ROOT#g" /etc/nginx/sites-available/site.conf
     sed -E -i "s#(<URL>)#$URL#g" /etc/nginx/sites-available/site.conf
     sed -E -i "s#(<INDEX>)#$INDEX#g" /etc/nginx/sites-available/site.conf
+    sed -E -i "s#(<CONTAINER>)#$CONTAINER#g" /etc/nginx/sites-available/site.conf
     ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
+
+    if [ -z $REMOTE_HOST ]; then
+        CONNECT_BACK='1'
+    else
+        CONNECT_BACK='0'
+    fi
+
+    sed -E -i "s#(<CONNECT_BACK>)#$CONNECT_BACK#g" /etc/php/7.2/mods-available/xdebug.ini
+    sed -E -i "s#(<REMOTE_HOST>)#$REMOTE_HOST#g" /etc/php/7.2/mods-available/xdebug.ini
+
 fi
 
 service php7.2-fpm start
